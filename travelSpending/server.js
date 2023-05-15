69dcb5086566f9b4164363647822004212145883
yang <kj8212123@gmail.com>
2023-05-15 18:30:10 +0800

    首頁OK 新增未完成

--- travelSpending/server.js
+++ travelSpending/server.js
@@ -15,17 +15,37 @@ var futureTravels =
         {id:1, countryName:'Philippines', date:'2023/7/7 - 2023/7/10',spend:'TWD $ 10,000'},
         {id:2, countryName:'Japan', date:'2023/8/5 - 2023/8/9',spend:'TWD $ 45,000'}
         // {id:1, countryName:'Philippines', date:'2023/7/7 - 2023/7/10'}
-    ]  
+    ] ;
 
+var pastTravels = 
+    [              
+        {id:1, countryName:'South-Korea', date:'2017/9/20 - 2017/10/8',spend:'TWD $ 20,000'},
+        {id:2, countryName:'United State', date:'2017/6/27 - 2017/9/18',spend:'TWD $ 20,000'}
+        // {id:1, countryName:'Philippines', date:'2023/7/7 - 2023/7/10'}
+    ] ;
+
+var countries = 
+    [
+        {name:'South-Korea',value:'south-korea'},
+        {name:'Japan',value:'japan'},
+        {name:'UK',value:'united-kingdom'},
+        {name:'United State',value:'united-states'}
+    ]
 
 app.get('/', (req, res) => {
-    console.log(futureTravels);
+    // console.log(futureTravels);
     res.render('home', {
-        futureTravels
+        futureTravels,
+        pastTravels
+        
     });
   });
 
-
+app.get('/add', (req,res) => {
+    res.render('add', {
+        countries
+    });
+})
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Hacker news server started on port: ${server.address().port}`);
