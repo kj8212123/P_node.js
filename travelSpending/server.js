d5e280c9aefc4c2001cda4b61e016a276efb05ae
yang <kj8212123@gmail.com>
2023-05-23 12:13:13 +0800

    datebase tarvelSpending

--- travelSpending/server.js
+++ travelSpending/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const mongoose = require('mongoose');
-const dbURL= 'mongodb+srv://aaaa1111:aaaa1111@nodetest.wsj9pmc.mongodb.net/?retryWrites=true&w=majority';
+const dbURL= 'mongodb+srv://aaaa1111:aaaa1111@nodetest.wsj9pmc.mongodb.net/travelSpending';
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 //將pug設為模板引擎
